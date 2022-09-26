f10f9f6b692e7a9f6eee92ce4b70d05fa9240074
rushikeshsawan <87987961+rushikeshsawan@users.noreply.github.com>
2022-09-26 13:44:08 +0530

    Update login.js

--- login.js
+++ login.js
@@ -66,15 +66,15 @@ const sslserver= https.createServer({
   cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
 },app)
 let port=4500;
-sslserver.listen(process.env.PORT || 5000,()=>{
-     console.log(`Listening to requests on http://localhost:${port}`);
-  }); 
+// sslserver.listen(process.env.PORT || 5000,()=>{
+//      console.log(`Listening to requests on http://localhost:${port}`);
+//   }); 
 // "192.168.0.104" || "localhost"
 // process.env.PORT || 5000
 // set app port
-// app.listen(port, process.env.PORT || 5000 ,() => {
-//   console.log(`Listening to requests on http://localhost:${port}`);
-// });
+app.listen(process.env.PORT || 5000 ,() => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
 
 // app.listen( port , ["192.168.0.104", "localhost" ],()=>{
 //    console.log(`Listening to requests on http://localhost:${port}`);
