a8a08d5877a03bd47e65ce02f53ba1178e46a591
rushikeshsawan <87987961+rushikeshsawan@users.noreply.github.com>
2022-09-26 16:41:57 +0530

    Update database.js

--- database.js
+++ database.js
@@ -9,19 +9,19 @@ const mysql= require("mysql");
 // });
 
 
-// const connection = mysql.createConnection({
-//     host: "sql6.freesqldatabase.com",
-//     user: "sql6521838",
-//     password: "89mNjBXtxi",
-//     database: "sql6521838"
-// });
-
 const connection = mysql.createConnection({
-    host: "sql.freedb.tech",
-    user: "freedb_rushikesh",
-    password: "z89g43px8sYw*dx",
-    database: "freedb_auticrushi"
+    host: "sql6.freesqldatabase.com",
+    user: "sql6521838",
+    password: "89mNjBXtxi",
+    database: "sql6521838"
 });
+
+// const connection = mysql.createConnection({
+//     host: "sql.freedb.tech",
+//     user: "freedb_rushikesh",
+//     password: "z89g43px8sYw*dx",
+//     database: "freedb_auticrushi"
+// });
  
 // connect to database
 connection.connect(function(error){ 
